94126221105fa71966980ba7233dced03aec4e8a
Alex <alexchurilov@mail.ru>
2022-07-14 11:21:08 +0300

    add Task_5

--- Task_5.py
+++ Task_5.py
@@ -5,12 +5,31 @@
 #- A (3,6); B (2,1) -> 5,09
 #- A (7,-5); B (1,-1) -> 7,21
 
+import math
 
-x1 = input("Введите x-координату 1й точки: ")
-y1 = input("Введите y-координату 1й точки: ")
-x2 = input("Введите x-координату 2й точки: ")
-y2 = input("Введите y-координату 2й точки: ")
+def input_coords(text):
 
-dist = ((x2 - x1)**2 + (y2-y1)**2)**0.5
+    int_test = True
+    is_minus = False
+    while int_test:
+        coord = input(f"{text}")
+        if coord[0] == "-":
+            is_minus = True
+            coord = coord.replace("-","")
+        if coord.isdigit():
+            coord = int(coord)
+            if is_minus:
+                coord *= -1
+            int_test = False
+        else:
+            print("Введенное значение не является числом")
+    return coord
 
-print(dist)
+x1 = input_coords("Введите x-координату 1й точки: ")
+y1 = input_coords("Введите y-координату 1й точки: ")
+x2 = input_coords("Введите x-координату 2й точки: ")
+y2 = input_coords("Введите y-координату 2й точки: ")
+
+dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+print(f"Расстояние между точками равно -> {dist:.2f}")
